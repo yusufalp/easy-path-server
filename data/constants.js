7c9eb81c5d68845219c5f28bedc3b697d34d991c
yusufalp <alpyusuf007@gmail.com>
2024-06-28 22:17:33 -0700

    build user schema

---
+++ data/constants.js
@@ -0,0 +1,10 @@
+export const validationRules = Object.freeze({
+  EMAIL_REGEX: /^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$/,
+  INVALID_EMAIL_MESSAGE: "Email is invalid, please enter a valid email!",
+  PASSWORD_REGEX:
+    /^(?=[A-Za-z0-9@#$%^&+!=]+$)^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[@#$%^&+!=]).*$/,
+  INVALID_PASSWORD_MESSAGE:
+    "Password invalid, it should contain an uppercase, a lowercase, a number and a special character!",
+  PASSWORD_LENGTH: 8,
+  INVALID_PASSWORD_LENGTH_MESSAGE: `Password must be at least ${PASSWORD_LENGTH} characters`,
+});
---
+++ models/userModel.js
@@ -0,0 +1,53 @@
+const mongoose = require("mongoose");
+
+const { Schema } = mongoose;
+
+const { validationRules } = require("../data/constants");
+
+const userSchema = new Schema(
+  {
+    name: {
+      first: {
+        type: String,
+        required: true,
+        trim: true,
+      },
+      last: {
+        type: String,
+        trim: true,
+      },
+    },
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+      lowercase: true,
+      match: [
+        validationRules.EMAIL_REGEX,
+        validationRules.INVALID_EMAIL_MESSAGE,
+      ],
+    },
+    password: {
+      type: String,
+      required: true,
+      match: [
+        validationRules.PASSWORD_REGEX,
+        validationRules.INVALID_PASSWORD_MESSAGE,
+      ],
+      minLength: [
+        validationRules.PASSWORD_LENGTH,
+        validationRules.INVALID_PASSWORD_LENGTH_MESSAGE,
+      ],
+    },
+    roles: [{ type: String }],
+    archived: { type: Boolean, default: false },
+    strategy: {
+      google: { id: { type: String, default: "" } },
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
